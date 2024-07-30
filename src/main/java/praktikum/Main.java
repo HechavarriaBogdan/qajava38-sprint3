fc0b1959766934fe80a73ea54eedb73a67b35c37
Bogdan Diakov <bogdandiakov@gmail.com>
2024-07-30 21:07:42 +0500

    add Modify conflict five

--- src/main/java/praktikum/Main.java
+++ src/main/java/praktikum/Main.java
@@ -3,7 +3,7 @@ package praktikum;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-	System.out.println("Another path_four");
-	System.out.println("Another path_one");
+	    System.out.println("Another path_four");
+	    System.out.println("Another path_one");
     }
 }
